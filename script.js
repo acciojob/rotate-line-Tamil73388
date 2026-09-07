4813f1b53c735bd420dd34f9224056046738d640
tamil <93432998+Tamil73388@users.noreply.github.com>
2026-09-07 14:57:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+let line = document.getElementById("line");
+let angle = 0;
+
+setInterval(() => {
+    angle += 2;
+    line.style.transform = `rotate(${angle}deg)`;
+}, 20);
